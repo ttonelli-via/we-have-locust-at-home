2ba193d5ef19fd2f5907c2d44d8c3632be5a4d10
madube-via <madube@viascience.com>
2024-10-21 16:32:24 -0400

    Adding keycloak logic

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"log/slog"
 	"net/http"
@@ -12,6 +14,10 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 var nodeUrls []string = []string{
@@ -27,6 +33,13 @@ var mode string
 var jsonBody string
 var requestsPerSecond int
 
+const (
+	BaseUmUrl = "keycloak.um-dev.dev.poweredbyvia.com"
+	Realm     = "tac-dev"
+	Region    = "ca-central-1"
+	SecretArn = "arn:aws:secretsmanager:ca-central-1:977445517197:secret:keycloak_dev_user_credentials-CFTeIU"
+)
+
 func init() {
 	flag.StringVar(&keycloakToken, "keycloak-token", "", "keycloak token to be included in headers")
 	flag.StringVar(&mode, "mode", "commit", "mode for each transaction submission")
@@ -41,8 +54,8 @@ func main() {
 		log.Fatal("no keycloak secret provided\n")
 	}
 
-	if mode != "commit" && mode != "sync" && mode != "async" {
-		log.Fatalf("%s is not a valid mode. choose from `commit`, `sync` or `async`.\n", mode)
+	if mode != "commit" && mode != "sync" && mode != "async" && mode != "adtm" {
+		log.Fatalf("%s is not a valid mode. choose from `commit`, `sync`, `async` or `adtm`.\n", mode)
 	}
 
 	appCtx, cancelFunc := context.WithCancel(context.Background())
@@ -56,13 +69,134 @@ func main() {
 	}()
 
 	for i, url := range nodeUrls {
-		go getEm(url, i+1, appCtx)
+		var endpoint string
+		// Check if mode is one of commit, sync, or async
+		if mode == "commit" || mode == "sync" || mode == "async" {
+			endpoint = fmt.Sprintf("%svsc/api/v1/transactions/?mode=%s", url, mode)
+		} else if mode == "adtm" {
+			// Use /arb-data-tm/ for mode adtm
+			endpoint = fmt.Sprintf("%sarb-data-tm/", url)
+		}
+		go getEm(endpoint, i+1, appCtx)
 	}
 
 	<-appCtx.Done()
 }
 
+// Struct for secret
+type KeycloakCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Function to retrieve secret from AWS Secrets Manager
+func getKeycloakUserSecret(appCtx context.Context) (KeycloakCredentials, error) {
+	//secretName := "keycloak_dev_user_credentials-CFTeIU"
+	region := "ca-central-1"
+
+	config, err := config.LoadDefaultConfig(appCtx, config.WithRegion(region), config.WithSharedConfigProfile("vianeer"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create Secrets Manager client
+	svc := secretsmanager.NewFromConfig(config)
+
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(SecretArn),
+	}
+
+	result, err := svc.GetSecretValue(appCtx, input)
+	if err != nil {
+		// For a list of exceptions thrown, see
+		// https://<<{{DocsDomain}}>>/secretsmanager/latest/apireference/API_GetSecretValue.html
+		log.Fatalf("GetSecretValue error: %s\n", err)
+		log.Fatal(err.Error())
+	}
+
+	// Decrypts secret using the associated KMS key.
+	var secretString string = *result.SecretString
+
+	// Unmarshal the secret string into KeycloakCredentials struct
+	var credentials KeycloakCredentials
+	err = json.Unmarshal([]byte(secretString), &credentials)
+	if err != nil {
+		log.Fatalf("failed to unmarshal secret: %v", err)
+	}
+
+	return credentials, err
+}
+
+// Function to retrieve Keycloak token (without retries)
+func getKeycloakToken(appCtx context.Context) (string, error) {
+	// Construct URL and headers
+	umUrl := fmt.Sprintf("https://%s/auth/realms/%s/protocol/openid-connect/token", BaseUmUrl, Realm)
+	secret, err := getKeycloakUserSecret(appCtx)
+	if err != nil {
+		return "", fmt.Errorf("error fetching Keycloak credentials: %v", err)
+	}
+
+	// Prepare POST request data
+	data := fmt.Sprintf("username=%s&password=%s&grant_type=password&client_id=admin-cli",
+		secret.Username, secret.Password)
+	req, err := http.NewRequest("POST", umUrl, bytes.NewBuffer([]byte(data)))
+	if err != nil {
+		return "", fmt.Errorf("failed to create request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("request failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("non-200 response: %v", resp.StatusCode)
+	}
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	// Parse the JSON response
+	var result map[string]interface{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse JSON response: %v", err)
+	}
+
+	// Extract the access token
+	token, ok := result["access_token"].(string)
+	if !ok {
+		return "", fmt.Errorf("access token not found in response")
+	}
+
+	return token, nil
+}
+
+// Function to get Authorization headers with Bearer token
+func getKeycloakTokenHeader(appCtx context.Context) (map[string]string, error) {
+	token, err := getKeycloakToken(appCtx)
+	if err != nil {
+		return nil, err
+	}
+
+	return map[string]string{"Authorization": fmt.Sprintf("Bearer %s", token)}, nil
+}
+
 func getEm(url string, nodeNumber int, ctx context.Context) {
+
+	// Fetch the Keycloak token header
+	headers, err := getKeycloakTokenHeader(ctx)
+	if err != nil {
+		log.Fatalf("failed to get Keycloak token: %v", err)
+	}
+
 	ticker := time.NewTicker(time.Second / time.Duration(requestsPerSecond))
 outer:
 	for {
@@ -71,13 +205,17 @@ outer:
 			break outer
 		case <-ticker.C:
 			go func() {
+
 				reqBody := []byte(jsonBody)
+
 				req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(reqBody))
 				if err != nil {
 					wrapped := fmt.Errorf("unable to create request due to the following error: %w", err)
 					log.Fatalln(wrapped.Error())
 				}
-				req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", keycloakToken))
+				for key, value := range headers {
+					req.Header.Add(key, value)
+				}
 				req.Header.Add("Content-Type", "application/json")
 
 				client := &http.Client{}
@@ -89,7 +227,6 @@ outer:
 					log.Fatalln(wrapped.Error())
 				}
 				resTime := time.Since(now)
-
 				slog.Info(fmt.Sprintf("Node %d", nodeNumber), "code", res.StatusCode, "response-time", resTime)
 			}()
 		}
